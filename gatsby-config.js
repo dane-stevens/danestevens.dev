95dcf62a3e770a96c2352ab4a6d32b9577caf467
Dane Stevens <dane@dsmedia.ca>
2020-11-29 21:01:10 -0500

    remove posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,24 +19,24 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts/`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-page-creator",
-      options: {
-        path: `${__dirname}/src/posts`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `posts`,
+    //     path: `${__dirname}/src/posts/`,
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-plugin-page-creator",
+    //   options: {
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
-          posts: require.resolve("./src/components/Layout/posts-layout.js"),
+          // posts: require.resolve("./src/components/Layout/posts-layout.js"),
           default: require.resolve("./src/components/Layout/default-page-layout.js"),
         },
       },
